703666eada8b95228f96e1906978194a04ade205
Kumaraswamy B G <71964026+XomaDev@users.noreply.github.com>
2021-02-27 16:15:41 +0530

    Update [i].js

--- api/[i].js
+++ api/[i].js
@@ -2,15 +2,8 @@ module.exports = (req, res) => {
   const inputString = req.query.i;
   res.statusCode = 200
 
-  var url = 'https://script.google.com/macros/s/AKfycbyxUhUuSDlhW3j7SBfknwb6L7t_6Z-GhpACJ5yCoG2_nVZ8oXE/exec?tag=' + inputString;
-  const request = require('request');
+  var url = "https://script.google.com/macros/s/AKfycbyxUhUuSDlhW3j7SBfknwb6L7t_6Z-GhpACJ5yCoG2_nVZ8oXE/exec?tag=" + inputString;
+  var index = httpGet(url) + "";
 
-  request('https://script.google.com/macros/s/AKfycbyxUhUuSDlhW3j7SBfknwb6L7t_6Z-GhpACJ5yCoG2_nVZ8oXE/exec?tag=' + inputString, { json: true }, (err, res, body) => {
-    if (err) { return console.log(err); }
-    console.log(body.url);
-    console.log(body.explanation);
-  });
-
-
-  res.json({ result: index })
+  res.json({ result: inputString.toString() })
 }
