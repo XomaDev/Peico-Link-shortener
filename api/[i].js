ee2909a838321369722e3a156e659331cbb89511
Kumaraswamy B G <71964026+XomaDev@users.noreply.github.com>
2021-02-27 16:05:07 +0530

    Update [i].js

--- api/[i].js
+++ api/[i].js
@@ -1,5 +1,10 @@
 module.exports = (req, res) => {
   const inputString = req.query.i;
   res.statusCode = 200
-  res.json({ result: inputString.toString() })
+
+  var url = "https://script.google.com/macros/s/AKfycbyxUhUuSDlhW3j7SBfknwb6L7t_6Z-GhpACJ5yCoG2_nVZ8oXE/exec?tag=" + inputString;
+  var index = httpGet(url) + "";
+
+
+  res.json(index)
 }
