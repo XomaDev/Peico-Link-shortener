87077c121f1f6add8ae3adca597a4bf7360f97cb
Hrichik Mazumder <57860916+hrichiksite@users.noreply.github.com>
2021-02-28 17:30:44 +0530

    Serverless API Redirect

--- api/[i].js
+++ api/[i].js
@@ -1,5 +1,17 @@
-module.exports = (req, res) => {
-  const inputString = req.query.i;
-  res.statusCode = 200
-  res.json({ result: inputString.toString() })
+const fetch = require('node-fetch');
+
+ module.exports = async (req, res) => {
+  const tag = req.query.tag;
+        if (tag != null) {
+        var url = "https://script.google.com/macros/s/AKfycbyxUhUuSDlhW3j7SBfknwb6L7t_6Z-GhpACJ5yCoG2_nVZ8oXE/exec?tag=" + tag;
+            var request = await fetch(url);
+            var response = await request.text();
+            if(response != null && response != "FAILED"){
+             res.setHeader('Cache-Control', 's-maxage=3155695200000')
+             res.setHeader("Location", res);
+             res.status(200).send();
+            } else {
+               res.send("Failed To Find Link");
+            }
+        }
 }
