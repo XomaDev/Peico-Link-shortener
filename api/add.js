65870a8bf9432a8d8e777fb3c9df49b2c7abcde6
Hrichik Mazumder <57860916+hrichiksite@users.noreply.github.com>
2021-03-03 17:14:43 +0530

    Added Security Updates
    * Security Update 1
    
    * Add URL adding API
    
    * Add API to Add URL
    
    * Add Package "google-safe-browsing"
    
    * Add Security Update 2
    
    * Update - Fixed Errors
    
    * Update Notice
    
    * Update index.html
    
    * Update Toasts
    
    * Update index.html
    
    * Update index.html
    
    * Update index.html
    
    * Update index.html
    
    * Update index.html
    
    * Delete generate.js
    
    * Chage To PROD URL
    
    * Security Features Update Complete

---
+++ api/add.js
@@ -0,0 +1,30 @@
+const fetch = require('node-fetch');
+const lookup = require('safe-browse-url-lookup')({ apiKey: 'AIzaSyDgjoHEfUjfZeIlUGOFEgCRdNKUmGNSlb8' });
+
+
+ module.exports = async (req, res) => {
+  const tag = req.query.tag;
+  const murl = req.query.url;
+
+        if (tag != null) {
+         lookup.checkSingle(murl)
+    .then(async isMalicious => {
+          if(isMalicious){
+            res.send("This URL seems Evil");
+          } else {
+            var url = "https://script.google.com/macros/s/AKfycbzM1oZleq9tCEfMO_AtXu69JwkycT41252ihP6uCqQDb0WsYAq3/exec?TAG=" + tag + "&URL=" + murl;
+            var request = await fetch(url);
+            var response = await request.text();
+            if(response != null && response != "FAILED" ){
+               res.send("200");
+            } else {
+               res.send("Failed To Add Link");
+            }};
+    }).catch(err => {
+               res.send("Failed To Scan Link");
+
+    })
+         
+
+}
+ }
