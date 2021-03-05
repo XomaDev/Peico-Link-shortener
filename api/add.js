b60889bf6c687d59fd949f4bb8f09bcb4c6c34e7
Hrichik Mazumder <57860916+hrichiksite@users.noreply.github.com>
2021-03-05 15:25:06 +0530

    Add new system to API
    Generating Tags At Google Script To Prevent Custom Tags

--- api/add.js
+++ api/add.js
@@ -3,22 +3,21 @@ const lookup = require('safe-browse-url-lookup')({ apiKey: 'AIzaSyDgjoHEfUjfZeIl
 
 
  module.exports = async (req, res) => {
-  const tag = req.query.tag;
   const murl = req.query.url;
   res.setHeader('Access-Control-Allow-Origin', 'https://s.peico.xyz')
   res.setHeader('Access-Control-Allow-Methods', 'GET')
 
-        if (tag != null) {
+        if (murl != null) {
          lookup.checkSingle(murl)
     .then(async isMalicious => {
           if(isMalicious){
             res.send("This URL seems Evil");
           } else {
-            var url = "https://script.google.com/macros/s/AKfycbzM1oZleq9tCEfMO_AtXu69JwkycT41252ihP6uCqQDb0WsYAq3/exec?TAG=" + tag + "&URL=" + murl;
+            var url = "https://script.google.com/macros/s/AKfycbzM1oZleq9tCEfMO_AtXu69JwkycT41252ihP6uCqQDb0WsYAq3/exec?URL=" + murl;
             var request = await fetch(url);
             var response = await request.text();
             if(response != null && response != "FAILED" ){
-               res.send("200");
+               res.send(response);
             } else {
                res.send("Failed To Add Link");
             }};
