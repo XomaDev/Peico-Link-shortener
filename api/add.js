c231e6db171e1ca05abddac54b79d9d86dbe5487
Kumaraswamy B G <71964026+XomaDev@users.noreply.github.com>
2021-03-09 20:00:34 +0530

    Update add.js

--- api/add.js
+++ api/add.js
@@ -7,20 +7,8 @@ const lookup = require('safe-browse-url-lookup')({ apiKey: 'AIzaSyDgjoHEfUjfZeIl
   console.log(murl);
   res.setHeader('Access-Control-Allow-Origin', 'https://s.peico.xyz')
   res.setHeader('Access-Control-Allow-Methods', 'GET')
-
         if (murl != null && murl != undefined) {
-         if(murl === "https://www.youtube.com/watch?v=dQw4w9WgXcQ" || murl === "https://youtu.be/dQw4w9WgXcQ"){
-            var url = "https://script.google.com/macros/s/AKfycbxEbbbhJFQlKTDSXsQfELqxuFPFeHTaT4AeEw_ETZDcpfYnTcE/exec?URL=rickroll";
-            var request = await fetch(url);
-            var response = await request.text();
-            if(response != "FAILED"){
-               res.send(response);
-            } else {
-               res.send("Failed To Add Link");
-            }
-         }
-         lookup.checkSingle(murl)
-    .then(async isMalicious => {
+         lookup.checkSingle(murl).then(async isMalicious => {
           if(isMalicious){
             res.send("This URL seems not safe. For security reasons we have blocked it.");
           } else {
@@ -35,12 +23,9 @@ const lookup = require('safe-browse-url-lookup')({ apiKey: 'AIzaSyDgjoHEfUjfZeIl
             }
          };
     }).catch(err => {
-               res.send("Opps, something went wrong! Error at: 2");
-
+            res.send("Opps, something went wrong! Error at: 2");
     })
-         
-
 } else { 
    res.send("Before we fall into the pastly undefined possible error, we would like to tell you <br><h1>THIS IS A API AND NEEDS A VALID QUERRYSTRING, and allows requests from s.peico.xyz</h1>");
-}
+  }
  }
