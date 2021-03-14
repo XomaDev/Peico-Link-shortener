288e61857c85509aee829a324c47bfdc3f5604ec
Hrichik Mazumder <57860916+hrichiksite@users.noreply.github.com>
2021-03-14 17:00:43 +0530

    Update add.js

--- api/add.js
+++ api/add.js
@@ -8,9 +8,12 @@ const lookup = require('safe-browse-url-lookup')({ apiKey: 'AIzaSyDgjoHEfUjfZeIl
   res.setHeader('Access-Control-Allow-Origin', 'https://s.peico.xyz')
   res.setHeader('Access-Control-Allow-Methods', 'GET')
         if (murl != null && murl != undefined) {
+         if(murl.includes("peico.xyz")){
+                         res.send("You can't shorten a already shortened URL");
+         }
          lookup.checkSingle(murl).then(async isMalicious => {
           if(isMalicious){
-            res.send("This URL seems not safe. For security reasons we have blocked it.");
+            res.send("This URL seems Evil");
           } else {
             var tag = Math.random().toString(36).toUpperCase().substr(3, 5);  
             var url = "https://script.google.com/macros/s/AKfycbxEbbbhJFQlKTDSXsQfELqxuFPFeHTaT4AeEw_ETZDcpfYnTcE/exec?URL=" + murl + "&TAG=" + tag;
@@ -19,11 +22,11 @@ const lookup = require('safe-browse-url-lookup')({ apiKey: 'AIzaSyDgjoHEfUjfZeIl
             if(response == "Done"){
                res.send(tag);
             } else {
-               res.send("Opps, something went wrong! Error at: 1");
+               res.send("Opps, something went wrong!");
             }
          };
     }).catch(err => {
-            res.send("Opps, something went wrong! Error at: 2");
+            res.send("Opps, something went wrong!");
     })
 } else { 
    res.send("Before we fall into the pastly undefined possible error, we would like to tell you <br><h1>THIS IS A API AND NEEDS A VALID QUERRYSTRING, and allows requests from s.peico.xyz</h1>");
